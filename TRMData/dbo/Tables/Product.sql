5406406d1f7fb0233ab281d83ec2705df82c1b01
Abdoulaye Dong <abdoulayedong@gmail.com>
2021-04-28 12:24:35 +0100

    Register DB Tables
    The tables for the basic register system.

---
+++ TRMData/dbo/Tables/Product.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Product]
+(
+	[Id] INT NOT NULL PRIMARY KEY identity,
+	[ProductName] nvarchar(50) not null,
+	[Description] nvarchar(Max) null,
+	[RetailPrice] money not null,
+	[CreateDate] datetime2 not null default getutcdate(),
+	[LastModified] datetime2 not null default getutcdate()
+)
---
+++ TRMData/dbo/Tables/Sale.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Sale]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[CashierId] nvarchar(128) not null,
+	[SaleDate] datetime2 not null,
+	[SubTotal] money not null,
+	[Tax] money not null,
+	[Total] money not null
+)
